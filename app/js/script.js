2ad0bd750e50bd0ca1716ee92fd81667e70968f0
Laura Krayacich <laura@krayacich.com.ar>
2019-09-17 19:49:12 -0300

    if two cards already flipped, flip them back

--- app/js/script.js
+++ app/js/script.js
@@ -7,67 +7,71 @@ const grid = document.querySelector('.grid');
 //Need to get this info from the click event
 const currentLvl = 1;
 
-//Create card div with back image
-const card = document.createElement('div');
-card.classList.add('card');
-const cardBack = document.createElement('img');
-cardBack.src = './app/img/question-mark.png';
-card.appendChild(cardBack);
-console.log(card);
 
-//Create deck array with all image urls
-let deck = [];
-for (let i = 0; i < AmountOfCardsbyLvl[currentLvl]; i++) {
-  deck.push("https://api.adorable.io/avatars/" + i);
+function setBoard() {
+
+  //Create deck array with all image urls
+  let deck = [];
+  for (let i = 0; i < AmountOfCardsbyLvl[currentLvl]; i++) {
+    deck.push("https://api.adorable.io/avatars/" + i);
+  }
+
+  //Duplicate elements in deck and shuffle it
+  deck = deck.concat(deck).sort(function () {
+    return 0.5 - Math.random();
+  });
+
+  //Append cards to grid
+  deck.forEach(function (elem) {
+
+    //Create card div
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    //Create card back, add src and class
+    const cardBack = document.createElement('img');
+    cardBack.src = './app/img/question-mark.png';
+    cardBack.classList.add('card__side');
+    cardBack.classList.add('card__side--back');
+
+    //Create card front, add src and class
+    const cardFront = document.createElement('img');
+    cardFront.src = elem;
+    cardFront.classList.add('card__side');
+    cardFront.classList.add('card__side--front');
+
+    //Append back and front to card
+    card.appendChild(cardBack);
+    card.appendChild(cardFront);
+
+    //Append card to grid
+    grid.appendChild(card);
+
+  });
 }
 
-//Duplicate elements in deck and shuffle it
-deck = deck.concat(deck).sort(function () {
-  return 0.5 - Math.random();
-});
-
-//Append cards to grid
-deck.forEach(function (elem) {
-
-  //Create card div
-  const card = document.createElement('div');
-  card.classList.add('card');
-
-  //Create card back, add src and class
-  const cardBack = document.createElement('img');
-  cardBack.src = './app/img/question-mark.png';
-  cardBack.classList.add('card__side');
-  cardBack.classList.add('card__side--back');
-
-  //Create card front, add src and class
-  const cardFront = document.createElement('img');
-  cardFront.src = elem;
-  cardFront.classList.add('card__side');
-  cardFront.classList.add('card__side--front');
-
-  //Append back and front to card
-  card.appendChild(cardBack);
-  card.appendChild(cardFront);
-
-  //Append card to grid
-  grid.appendChild(card);
-
-});
-
-
-console.log(deck);
-
-/* function setBoard() {
-
-} */
+setBoard();
 
 let cards = document.querySelectorAll('.card');
+
+let cardsFlipped = document.querySelectorAll('.card--flipped');
+console.log(cardsFlipped);
 
 cards.forEach(function (elem) {
 
   elem.addEventListener('click', function () {
 
-    elem.classList.toggle('card--flipped');
+    if (!cardsFlipped || cardsFlipped.length < 2) {
+      elem.classList.toggle('card--flipped');
+      cardsFlipped = document.querySelectorAll('.card--flipped');
+      console.log(cardsFlipped);
+    } else {
+      cardsFlipped.forEach(function (elem) {
+        elem.classList.remove('card--flipped');
+      });
+      cardsFlipped = undefined;
+      console.log(cardsFlipped);
+    }
 
   });
 
@@ -78,5 +82,6 @@ cards.forEach(function (elem) {
 } */
 
 /* function checkMatch() {
+
 
 } */
